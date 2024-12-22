f75e8d4d4be9d7a295afa264fd9c1043f36296f5
khadaryaree <khadarmohamed062@gmai.com>
2024-12-22 13:13:50 +0200

    first commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,11 +8,7 @@ export const About = () => {
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
-        <img
-          src={getImageUrl("about/aboutImage.png")}
-          alt="Me sitting with a laptop"
-          className={styles.aboutImage}
-        />
+       
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
@@ -22,7 +18,7 @@ export const About = () => {
                 I'm a frontend developer with experience in building responsive
                 and optimized sites
               </p>
-            </div>
+            </div>  
           </li>
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,17 +7,18 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm  khadar</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
-        </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+I am a final-year full-stack development student with 3 years of hands-on experience in building dynamic and responsive web applications using React and Node.js. 
+My expertise includes developing user-friendly interfaces, optimizing backend performance, and collaborating with teams to deliver scalable solutions.
+I'm passionate about learning new technologies and applying innovative approaches to solve complex problems. Feel free to reach out if you'd like to connect or discuss opportunities!"
+</p>
+        <a href="khadarmohamed062@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/khadar.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
